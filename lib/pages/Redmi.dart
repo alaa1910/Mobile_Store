541890d6908bac7ac0f1825aa09f7bedbde6fbbe
nehal <nehalgamal63@gmail.com>
2020-11-03 22:07:35 +0200

    addphotoIPhone&Add icon in mydrawer.dart

--- lib/pages/Redmi.dart
+++ lib/pages/Redmi.dart
@@ -28,13 +28,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p1.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:3200 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -46,13 +46,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p2.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :2 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:4200 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -64,13 +64,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p11.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :2 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1600 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -82,13 +82,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p3.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :15 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:2200 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -100,13 +100,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p4.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :2 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:6000 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -118,13 +118,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p5.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :16 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:5200 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -136,13 +136,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p6.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :4 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:6200 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -154,13 +154,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p7.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:2),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:4200 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -172,13 +172,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p8.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :6 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:3200 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -190,13 +190,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p9.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :64 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:7200 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
@@ -208,13 +208,13 @@ class RedmiState extends State<Redmi>{
               child: Row(
 
                 children:<Widget> [
-                  Expanded(flex:1,child: Image.asset("images/download(S).jpg",fit:BoxFit.cover)),
-                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:5),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
+                  Expanded(flex:1,child: Image.asset("images/Redmi/p10.jpg",fit:BoxFit.cover)),
+                  Expanded(flex:2,child:Container(padding: EdgeInsets.only(right:3),alignment: Alignment.topRight,height: 100,child:Column(crossAxisAlignment: CrossAxisAlignment.start,children: [Text("المواصفات",style: TextStyle(fontWeight: FontWeight.w800),textAlign: TextAlign.center,),
                     Row(children: [
-                      Expanded(child:Text("الكاميرا :5 ميجابيت",style: TextStyle(color:Colors.grey),)),
-                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.grey),)
+                      Expanded(child:Text("الكاميرا :22 ميجابيت",style: TextStyle(color:Colors.orange),)),
+                      Text("المعالج : ثماني النواة",style: TextStyle(color:Colors.orange),)
                     ],),
-                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:1200 جنيه",style: TextStyle(color:Colors.red),),)
+                    Container(margin: EdgeInsets.only(top:10),child:Text("السعر:5000 جنيه",style: TextStyle(color:Colors.blue),),)
                   ],),) ),
                 ],
               ),)),
